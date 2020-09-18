2a9aa169db28552b1b9d0f2f5b7cc22d4dad7398
John Munsey <see3dee@gmail.com>
2020-09-18 15:38:04 -0700

    refactor with neg test

--- spec.js
+++ spec.js
@@ -4,31 +4,26 @@ describe('Protractor Demo App', function() {
     var secondNumber = element(by.model('second'));
     var goButton = element(by.id('gobutton'));
     var latestResult = element(by.binding('latest'));
+    var history = element.all(by.repeater('result in memory'));
+
+    function add(a, b) {
+        firstNumber.sendKeys(a);
+        secondNumber.sendKeys(b);
+        goButton.click();
+    }
 
     beforeEach(function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
     });
 
-    it('should have a title', function() {
-        expect(browser.getTitle()).toEqual('Super Calculator');
-    });
+    it('should have a history', function() {
+        add(1, 2);
+        add(3, 4);
 
-    it('should add one and two', function() {
-        firstNumber.sendKeys(1);
-        secondNumber.sendKeys(2);
+        expect(history.count()).toEqual(2);
 
-        goButton.click();
+        add(5, 6);
 
-        expect(latestResult.getText()).toEqual('3');
-    });
-
-    it('should add four and six', function() {
-        // Fill this in.
-        expect(latestResult.getText()).toEqual('10');
-    });
-
-    it('should read the value from an input', function() {
-        firstNumber.sendKeys(1);
-        expect(firstNumber.getAttribute('value')).toEqual('1');
+        expect(history.count()).toEqual(0); // This is wrong!
     });
 });
